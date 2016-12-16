f217de06438b043b6569f0c6af94982c039d045f
Oleg Shatin 11-501 <mongoose0911@gmail.com>
2016-12-16 11:56:52 +0300

    added Client Interface, Client Impl, NetMethod enum, Server class. written Controllable interface

--- src/ru/kpfu/group11501/airhockey/controller/Controller.java
+++ src/ru/kpfu/group11501/airhockey/controller/Controller.java
@@ -1,4 +1,40 @@
 package ru.kpfu.group11501.airhockey.controller;
 
-public class Controller {
+import ru.kpfu.group11501.airhockey.net.Client;
+
+public class Controller implements Client {
+    @Override
+    public void updatePuckDirection(Double puckX, Double puckY) {
+
+    }
+
+    @Override
+    public void updateGameScore(Integer clientScore, Integer opponentScore) {
+
+    }
+
+    @Override
+    public void updateOpponentMalletDirection(Double malletX, Double malletY) {
+
+    }
+
+    @Override
+    public void setGameResult(Integer clientScore, Integer opponentScore) {
+
+    }
+
+    @Override
+    public void gameStartsInTime(Long timeInstanceOfStart) {
+
+    }
+
+    @Override
+    public void opponentIsReady() {
+
+    }
+
+    @Override
+    public void opponentLeftGame() {
+
+    }
 }
